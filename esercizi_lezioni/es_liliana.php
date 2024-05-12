0b53d38b89ac2922a033f4e416c22024fa7596a1
CristinaEnas <enascristina33@gmail.com>
2024-05-12 11:18:08 +0200

    elercizi

---
+++ esercizi_lezioni/es_liliana.php
@@ -0,0 +1,4 @@
+<?php
+// ! Inserite da terminale due numeri e calcolare la loro somma, la sottrazione,
+// la moltiplicazione o la divisione (per la divisione controllate che il divisore
+// sia diverso da zero, prima di effettuare l’operazione).
